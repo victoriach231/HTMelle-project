0ade3db2d07ddf33ce52a9df46251f2f4da5acbb
Victoria C <victoriach231@gmail.com>
2019-03-09 03:26:21 +0000

    lots o files

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function contactFunction() {
+    
+}
 document.getElementById("shopBID").addEventListener("click", myFunction);
+document.getElementById("cButton").addEventListener("click", contactFunction);
